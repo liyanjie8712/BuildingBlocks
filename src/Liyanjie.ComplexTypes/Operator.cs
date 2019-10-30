7661054ecbbe8241cd55c008ca5f01b707d4ec8e
李彦杰 <liyanjie8712@outlook.com>
2019-10-30 19:08:56 +0800

    +

--- src/Liyanjie.ComplexTypes/Operator.cs
+++ src/Liyanjie.ComplexTypes/Operator.cs
@@ -6,13 +6,13 @@ namespace Liyanjie.ComplexTypes
     /// <summary>
     /// 操作人
     /// </summary>
-    /// <typeparam name="TType"></typeparam>
-    public class Operator<TType> : ValueObject
+    /// <typeparam name="TStatus"></typeparam>
+    public class Operator<TStatus> : ValueObject
     {
         /// <summary>
         /// 类型
         /// </summary>
-        public TType Type { get; set; }
+        public TStatus Status { get; set; }
 
         /// <summary>
         /// 标识
@@ -27,10 +27,24 @@ namespace Liyanjie.ComplexTypes
         /// <summary>
         /// 
         /// </summary>
+        /// <param name="status"></param>
+        /// <param name="identity"></param>
+        /// <param name="remark"></param>
+        /// <returns></returns>
+        public static Operator<TStatus> Create(TStatus status, Guid? identity, string remark = default) => new Operator<TStatus>
+        {
+            Status = status,
+            Identity = identity,
+            Remark = remark,
+        };
+
+        /// <summary>
+        /// 
+        /// </summary>
         /// <returns></returns>
         protected override IEnumerable<object> GetAtomicValues()
         {
-            yield return Type;
+            yield return Status;
             yield return Identity;
             yield return Remark;
         }
