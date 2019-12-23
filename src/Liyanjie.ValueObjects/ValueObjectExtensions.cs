ef1b488371ae3b318aac0b3031e1e24018babbde
李彦杰 <liyanjie8712@outlook.com>
2019-12-23 13:35:50 +0800

    +

--- src/Liyanjie.ValueObjects/ValueObjectExtensions.cs
+++ src/Liyanjie.ValueObjects/ValueObjectExtensions.cs
@@ -14,20 +14,24 @@ namespace Liyanjie.ValueObjects
         /// <param name="origin"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        public static T Assign<T>(this T origin, T value)
+        public static void Assign<T>(this T origin, T value)
             where T : ValueObject
         {
-            var type = origin.GetType();
-            var type_ComplexType = typeof(ValueObject);
-            var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            foreach (var property in properties)
+            if (origin == null || value == null)
+                return;
+            else
             {
-                if (type_ComplexType.IsAssignableFrom(property.PropertyType))
-                    (property.GetValue(origin) as ValueObject).Assign(property.GetValue(value) as ValueObject);
-                else if (property.CanWrite && value != null)
-                    property.SetValue(origin, property.GetValue(value));
+                var type = origin.GetType();
+                var type_ComplexType = typeof(ValueObject);
+                var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                foreach (var property in properties)
+                {
+                    if (type_ComplexType.IsAssignableFrom(property.PropertyType))
+                        Assign(property.GetValue(origin) as ValueObject, property.GetValue(value) as ValueObject);
+                    else if (property.CanWrite && value != null)
+                        property.SetValue(origin, property.GetValue(value));
+                }
             }
-            return origin;
         }
     }
 }
