d3b3c06572f14bfc83f1603d290ed44c09eb57f1
liyanjie <liyanjie8712@outlook.com>
2020-08-24 16:52:52 +0800

    +

--- src/Liyanjie.Drawing.Extensions/ImageExtensions.cs
+++ src/Liyanjie.Drawing.Extensions/ImageExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
+﻿using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 
---
+++ src/Liyanjie.Utilities/RandomExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace System
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class RandomExtensions
+    {
+        /// <summary>
+        /// 随机的Boolean值
+        /// </summary>
+        /// <param name="random"></param>
+        /// <returns></returns>
+        public static bool NextBool(this Random random)
+        {
+            return random.NextDouble() < 0.5D;
+        }
+    }
+}
