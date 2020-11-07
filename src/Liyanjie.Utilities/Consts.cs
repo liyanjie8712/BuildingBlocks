91a637650f4890fd1c648150722f69c2aebf4681
liyanjie <liyanjie8712@outlook.com>
2020-11-07 12:03:17 +0800

    +

--- src/Liyanjie.Utilities/Consts.cs
+++ src/Liyanjie.Utilities/Consts.cs
@@ -6,8 +6,13 @@
     public static class Consts
     {
         /// <summary>
-        /// 
+        /// IPv4正则
         /// </summary>
         public const string RegexPattern_IPv4 = @"([1-9]?\d|1\d{2}|2([0-4]\d|5[0-5]))(\.([1-9]?\d|1\d{2}|2([0-4]\d|5[0-5]))){3}";
+
+        /// <summary>
+        /// E-Mail正则
+        /// </summary>
+        public const string RegexPattern_Email = @"(\w)+(\.\w+)*@(\w)+((\.\w+)+)";
     }
 }
