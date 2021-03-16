f32a1fa79a6dbe5e48460c1839d5ebcc347fe307
李彦杰 <liyanjie8712@outlook.com>
2021-03-16 16:49:27 +0800

    +

---
+++ src/Liyanjie.AspNetCore.Mvc.Extensions/UniqueAuthorizationFilter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Extensions
+{
+    public class UniqueAuthorizationFilter : IAuthorizationFilter
+    {
+        public const string ClaimType_UniqueId = "UniqueId";
+
+        readonly Func<AuthorizationFilterContext, string> getUserUniqueId;
+        public UniqueAuthorizationFilter(Func<AuthorizationFilterContext, string> getUserUniqueId)
+        {
+            this.getUserUniqueId = getUserUniqueId ?? throw new ArgumentNullException(nameof(getUserUniqueId));
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (true
+                && context.ActionDescriptor.FilterDescriptors.Any(_ => _ is IAuthorizeData)
+                && !context.ActionDescriptor.FilterDescriptors.Any(_ => _ is IAllowAnonymous)
+                && context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                var tokenUniqueId = context.HttpContext.User.Claims
+                    .SingleOrDefault(_ => _.Type == ClaimType_UniqueId)?.Value ?? Guid.NewGuid().ToString("N");
+                if (tokenUniqueId == null)
+                    goto Unauthorized;
+
+                var userUniqueId = getUserUniqueId.Invoke(context);
+                if (tokenUniqueId != userUniqueId)
+                    goto Unauthorized;
+
+                Unauthorized:
+                context.Result = new StatusCodeResult(401);
+            }
+        }
+    }
+}
